b4c72a80c68d543b6d68513e3064f6c9513df7f5
Abdurab <abdrb22@gmail.com>
2019-02-20 16:36:00 +0300

    modified all pages

--- js/links.js
+++ js/links.js
@@ -1,5 +1,9 @@
-function func(){
-document.getElementById("link1").innerHTML = "Home";
-document.getElementById("link1").href = "index.html";
+var website = "https://abdrb.github.io/";
 
+function func(){
+document.getElementById("links").innerHTML += "<li><a href='" + website + "index.html'>Home</a></li>";
+document.getElementById("links").innerHTML += "<li><a href='" + website + "services.html'>Our Services</a></li>";
+document.getElementById("links").innerHTML += "<li><a href='" + website + "about.html'>About Us</a></li>";
+document.getElementById("links").innerHTML += "<li><a href='" + website + "feedback/feedback.html'>Feedback</a></li>";
+document.getElementById("links").innerHTML += "<li><a href='" + website + "learning/arabic.htm'>Typing skill</a></li>";
 }
