ea66640866cbed57d606ac66b1df6af33228c1f2
Abdurab <abdrb22@gmail.com>
2019-02-20 23:39:33 +0300

    css creator

---
+++ js/cssg.js
@@ -0,0 +1,71 @@
+﻿var save1 = [];
+var save2 = [];
+
+function myFunction () {
+if(document.getElementById("tag").value === "" || document.getElementById("att").value === "" || document.getElementById("val").value === ""){
+	alert("يجب تعبئة الحقول");
+	return "";
+}
+	var type = document.getElementById("type").value;
+	var tag = document.getElementById("tag").value;
+	var att = document.getElementById("att").value;
+	var val = document.getElementById("val").value;
+	var nl = "\n";
+	var start = " {";
+	var end = "}";
+	var cssStyle = "";
+	var addNew = "";
+	var num = Number(document.getElementById("hN").value);
+if(type === "class"){
+	tag = "." + document.getElementById("tag").value;
+	}
+else if (type === "id"){
+	tag = "#" + document.getElementById("tag").value;
+	}
+else {
+	tag = document.getElementById("tag").value;
+	}
+
+for(var i = 1; i <= num; i++){
+	addNew += document.getElementById("att" + i).value + ": ";
+	addNew += document.getElementById("val" + i).value + ";\n";
+}
+if(Number(document.getElementById("hN").value) === 0) {
+	addNew = "";
+}
+cssStyle = tag + start + nl + att + ": " + val + ";" + nl + addNew + end + nl;
+document.getElementById("cssBox").value = cssStyle;
+}
+
+function func (){
+	var type = document.getElementById("type").value;
+if(type === "class"){
+	document.getElementById("slct").innerHTML = "أكتب اسم الفئة";
+}
+else if (type === "id"){
+	document.getElementById("slct").innerHTML = "أكتب اسم المعرف";
+}
+else {
+	document.getElementById("slct").innerHTML = "أكتب اسم الوسم";
+	}
+}
+
+function add(){
+var n = Number(document.getElementById("hN").value) + 1;
+if(n >= 2){
+save1[(n - 2)] = document.getElementById("att" + (n - 1)).value;
+save2[(n - 2)] = document.getElementById("val" + (n - 1)).value;
+}
+var txt = "<input list='lst' id='att" + n + "' class='inl'>" + "<input type='text' id='val" + n + "' class='inl'>";
+document.getElementById("hN").value = n;
+document.getElementById("newA").innerHTML += txt;
+for(var i = 2; i <= n; i++){
+document.getElementById("att" + (i - 1)).value = save1[(i - 2)];
+document.getElementById("val" + (i - 1)).value = save2[(i - 2)];
+}
+}
+
+function clr(){
+document.getElementById("newA").innerHTML = "";
+document.getElementById("hN").value = 0;
+}
--- js/links.js
+++ js/links.js
@@ -1,9 +1,9 @@
 var website = "https://abdrb.github.io/";
 
-function func(){
+function funclinks(){
 document.getElementById("links").innerHTML += "<li><a href='" + website + "index.html'>Home</a></li>";
 document.getElementById("links").innerHTML += "<li><a href='" + website + "services.html'>Our Services</a></li>";
 document.getElementById("links").innerHTML += "<li><a href='" + website + "about.html'>About Us</a></li>";
+document.getElementById("links").innerHTML += "<li><a href='" + website + "projects.html'>Apps</a></li>";
 document.getElementById("links").innerHTML += "<li><a href='" + website + "feedback/feedback.html'>Feedback</a></li>";
-document.getElementById("links").innerHTML += "<li><a href='" + website + "learning/arabic.htm'>Typing skill</a></li>";
 }
