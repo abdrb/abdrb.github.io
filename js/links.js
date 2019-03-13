b94dee7b3c86f02e9e14cba8976f01366676e16c
forker <abdrb22@yahoo.com>
2019-03-13 04:29:20 +0300

    add text

--- js/links.js
+++ js/links.js
@@ -3,7 +3,7 @@ var website = "https://abdrb.github.io/";
 function funclinks(){
 document.getElementById("links").innerHTML += "<li><a href='" + website + "index.html'>Home</a></li>";
 document.getElementById("links").innerHTML += "<li><a href='" + website + "services.html'>Expert</a></li>";
-document.getElementById("links").innerHTML += "<li><a href='" + website + "about.html'>About</a></li>";
+document.getElementById("links").innerHTML += "<li><a href='" + website + "about.html'>Effect</a></li>";
 document.getElementById("links").innerHTML += "<li><a href='" + website + "projects.html'>Apps</a></li>";
 document.getElementById("links").innerHTML += "<li><a href='" + website + "feedback/feedback.html'>Feedback</a></li>";
 }
