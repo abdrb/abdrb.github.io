d362df52016171895867244572acf3549c182d5d
Abdurab <abdrb22@gmail.com>
2019-04-06 21:18:39 +0300

    links

--- js/links.js
+++ js/links.js
@@ -1,6 +1,8 @@
-function func(){
-document.getElementById("link1").innerHTML = "Home";
-document.getElementById("link1").href = "index.html";
-
-}
-    
+var website = "https://abdrb.github.io/";
+function funclinks(){
+document.getElementById("links").innerHTML += "<li><a href='" + website + "index.html'>Home</a></li>";
+document.getElementById("links").innerHTML += "<li><a href='" + website + "services.html'>Expert</a></li>";
+document.getElementById("links").innerHTML += "<li><a href='" + website + "about.html'>Effect</a></li>";
+document.getElementById("links").innerHTML += "<li><a href='" + website + "projects.html'>Apps</a></li>";
+document.getElementById("links").innerHTML += "<li><a href='" + website + "feedback/feedback.html'>Feedback</a></li>";
+} 
