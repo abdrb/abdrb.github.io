18c13212dd8e51eb0d56887046d3b40f6667e05a
forker <abdrb22@yahoo.com>
2020-10-15 16:32:40 +0300

    fix navbar menu

--- js/links.js
+++ js/links.js
@@ -1,12 +1,13 @@
 var website = "https://abdrb.github.io/";
 function funclinks(){
 document.getElementById("links").innerHTML += "<li><a href='" + website + "index.html'>Home</a></li>";
-document.getElementById("links").innerHTML += "<li><a href='" + website + "services.html'>Expert</a></li>";
-document.getElementById("links").innerHTML += "<li><a href='" + website + "about.html'>Effect</a></li>";
-document.getElementById("links").innerHTML += "<li><a href='" + website + "projects.html'>Apps</a></li>";
+//document.getElementById("links").innerHTML += "<li><a href='" + website + "services.html'>Expert</a></li>";
+//document.getElementById("links").innerHTML += "<li><a href='" + website + "about.html'>Effect</a></li>";
+document.getElementById("links").innerHTML += "<li><a href='" + website + "typing.html'>Learn Typing</a></li>";
 document.getElementById("links").innerHTML += "<li><a href='" + website + "space/space.html'>Space App</a></li>";
-document.getElementById("links").innerHTML += "<li><a href='" + website + "space/probe.html'>Hope probe</a></li>";
+document.getElementById("links").innerHTML += "<li><a href='" + website + "space/probe.html'>Hope Probe</a></li>";
 document.getElementById("links").innerHTML += "<li><a href='" + website + "space/farmer.html'>Farmer</a></li>";
 document.getElementById("links").innerHTML += "<li><a href='" + website + "solar/solar.html'>Solar|WattAmper Calc</a></li>";
+document.getElementById("links").innerHTML += "<li><a href='" + website + "cssg.html'>CSS Style Generator</a></li>";
 document.getElementById("links").innerHTML += "<li><a href='" + website + "feedback.html'>Feedback</a></li>";
 } 
