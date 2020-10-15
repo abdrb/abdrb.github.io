8949e99736ccf556f1b54a5f87b33905e482043a
forker <abdrb22@yahoo.com>
2020-10-15 15:40:57 +0300

    locations list and links

--- js/links.js
+++ js/links.js
@@ -4,5 +4,9 @@ document.getElementById("links").innerHTML += "<li><a href='" + website + "index
 document.getElementById("links").innerHTML += "<li><a href='" + website + "services.html'>Expert</a></li>";
 document.getElementById("links").innerHTML += "<li><a href='" + website + "about.html'>Effect</a></li>";
 document.getElementById("links").innerHTML += "<li><a href='" + website + "projects.html'>Apps</a></li>";
+document.getElementById("links").innerHTML += "<li><a href='" + website + "space/space.html'>Space App</a></li>";
+document.getElementById("links").innerHTML += "<li><a href='" + website + "space/probe.html'>Hope probe</a></li>";
+document.getElementById("links").innerHTML += "<li><a href='" + website + "space/farmer.html'>Farmer</a></li>";
+document.getElementById("links").innerHTML += "<li><a href='" + website + "solar/solar.html'>Solar|WattAmper Calc</a></li>";
 document.getElementById("links").innerHTML += "<li><a href='" + website + "feedback.html'>Feedback</a></li>";
 } 
