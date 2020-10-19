21f418f9db19e3f71135b241a264d069fe4c2a20
forker <abdrb22@yahoo.com>
2020-10-19 10:32:55 +0300

    style improvements

--- js/links.js
+++ js/links.js
@@ -1,13 +1,28 @@
 var website = "https://abdrb.github.io/";
+var link = "";
+var social="";
+
 function funclinks(){
-document.getElementById("links").innerHTML += "<li><a href='" + website + "index.html'>Home</a></li>";
-//document.getElementById("links").innerHTML += "<li><a href='" + website + "services.html'>Expert</a></li>";
-//document.getElementById("links").innerHTML += "<li><a href='" + website + "about.html'>Effect</a></li>";
-document.getElementById("links").innerHTML += "<li><a href='" + website + "typing.html'>Learn Typing</a></li>";
-document.getElementById("links").innerHTML += "<li><a href='" + website + "space/space.html'>Space App</a></li>";
-document.getElementById("links").innerHTML += "<li><a href='" + website + "space/probe.html'>Hope Probe</a></li>";
-document.getElementById("links").innerHTML += "<li><a href='" + website + "space/farmer.html'>Farmer</a></li>";
-document.getElementById("links").innerHTML += "<li><a href='" + website + "solar/solar.html'>Solar|WattAmper Calc</a></li>";
-document.getElementById("links").innerHTML += "<li><a href='" + website + "cssg.html'>CSS Style Generator</a></li>";
-document.getElementById("links").innerHTML += "<li><a href='" + website + "feedback.html'>Feedback</a></li>";
-} 
+link += "<li><a href='" + website + "index.html'>Home</a></li>";
+//link += "<li><a href='" + website + "services.html'>Expert</a></li>";
+//link += "<li><a href='" + website + "about.html'>Effect</a></li>";
+link += "<li><a href='" + website + "typing.html'>Learn Typing</a></li>";
+link += "<li><a href='" + website + "space/space.html'>Space App</a></li>";
+link += "<li><a href='" + website + "space/probe.html'>Hope Probe</a></li>";
+link += "<li><a href='" + website + "space/farmer.html'>Farmer</a></li>";
+link += "<li><a href='" + website + "solar/solar.html'>Solar|WattAmper Calc</a></li>";
+link += "<li><a href='" + website + "cssg.html'>CSS Style Generator</a></li>";
+link += "<li><a href='" + website + "feedback.html'>Feedback</a></li>";
+document.getElementById("links").innerHTML=link;
+
+social += "<a href=''><i class='fa fa-codepen'></i></a>";
+social += "<a href=''><i class='fa fa-github'></i></a>";
+social += "<a href=''><i class='fa fa-linkedin-square'></i></a>";
+social += "<a href=''><i class='fa fa-facebook-square'></i></a>";
+social += "<a href=''><i class='fa fa-twitter'></i></a>";
+social += "<a href=''><i class='fa fa-youtube'></i></a>";
+social += "<a href=''><i class='fa fa-whatsapp'></i></a>";
+social += "<a href=''><i class='fa fa-instagram'></i></a>";
+social += "<a href='feedback.html'><i class='fa fa-envelope'></i></a>";
+document.getElementById("sociallinks").innerHTML=social;
+}
--- js/sociallinks.js
+++ js/sociallinks.js
@@ -1,23 +1,23 @@
 var website = "https://abdrb.github.io/";
 function funclinks(){
-document.getElementById("sociallinks").innerHTML += "<a href='" + website + "index.html'>Home</a></li>";
-document.getElementById("sociallinks").innerHTML += "<a href='" + website + "services.html'>Expert</a></li>";
-document.getElementById("sociallinks").innerHTML += "<a href='" + website + "about.html'>Effect</a></li>";
-document.getElementById("sociallinks").innerHTML += "<a href='" + website + "typing.html'>Learn Typing</a></li>";
-document.getElementById("sociallinks").innerHTML += "<a href='" + website + "space/space.html'>Space App</a></li>";
-document.getElementById("sociallinks").innerHTML += "<a href='" + website + "space/probe.html'>Hope Probe</a></li>";
-document.getElementById("sociallinks").innerHTML += "<a href='" + website + "space/farmer.html'>Farmer</a></li>";
-document.getElementById("sociallinks").innerHTML += "<a href='" + website + "solar/solar.html'>Solar|WattAmper Calc</a></li>";
-document.getElementById("sociallinks").innerHTML += "<a href='" + website + "cssg.html'>CSS Style Generator</a></li>";
-document.getElementById("sociallinks").innerHTML += "<a href='" + website + "feedback.html'>Feedback</a></li>";
-} 
+document.getElementById("links").innerHTML += "<a href='" + website + "index.html'>Home</a></li>";
+document.getElementById("links").innerHTML += "<a href='" + website + "services.html'>Expert</a></li>";
+document.getElementById("links").innerHTML += "<a href='" + website + "about.html'>Effect</a></li>";
+document.getElementById("links").innerHTML += "<a href='" + website + "typing.html'>Learn Typing</a></li>";
+document.getElementById("links").innerHTML += "<a href='" + website + "space/space.html'>Space App</a></li>";
+document.getElementById("links").innerHTML += "<a href='" + website + "space/probe.html'>Hope Probe</a></li>";
+document.getElementById("links").innerHTML += "<a href='" + website + "space/farmer.html'>Farmer</a></li>";
+document.getElementById("links").innerHTML += "<a href='" + website + "solar/solar.html'>Solar|WattAmper Calc</a></li>";
+document.getElementById("links").innerHTML += "<a href='" + website + "cssg.html'>CSS Style Generator</a></li>";
+document.getElementById("links").innerHTML += "<a href='" + website + "feedback.html'>Feedback</a></li>";
 
-"'><i class='fa fa-codepen"></i></a>
-"'><i class='fa fa-github"></i></a>
-"'><i class='fa fa-linkedin-square"></i></a>
-"'><i class='fa fa-facebook-square"></i></a>
-"'><i class='fa fa-twitter"></i></a>
-"'><i class='fa fa-youtube"></i></a>
-"'><i class='fa fa-whatsapp"></i></a>
-"'><i class='fa fa-instagram"></i></a>
-"'><i class='fa fa-envelope"></i></a>
+document.getElementById("sociallinks").innerHTML += "<a href='" + codepen + "'><i class='fa fa-codepen'></i></a>";
+document.getElementById("sociallinks").innerHTML += "<a href='" + github + "'><i class='fa fa-github'></i></a>";
+document.getElementById("sociallinks").innerHTML += "<a href='" + linkedin + "'><i class='fa fa-linkedin-square'></i></a>";
+document.getElementById("sociallinks").innerHTML += "<a href='" + facebook + "'><i class='fa fa-facebook-square'></i></a>";
+document.getElementById("sociallinks").innerHTML += "<a href='" + twitter + "'><i class='fa fa-twitter'></i></a>";
+document.getElementById("sociallinks").innerHTML += "<a href='" + youtube + "'><i class='fa fa-youtube'></i></a>";
+document.getElementById("sociallinks").innerHTML += "<a href='" + whatsapp + "'><i class='fa fa-whatsapp'></i></a>";
+document.getElementById("sociallinks").innerHTML += "<a href='" + instagram + "'><i class='fa fa-instagram'></i></a>";
+document.getElementById("sociallinks").innerHTML += "<a href='" + contact + "'><i class='fa fa-envelope'></i></a>";
+}
