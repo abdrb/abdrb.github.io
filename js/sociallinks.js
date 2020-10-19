f60408614333984ab9a08cee9a1228ba54a06702
forker <abdrb22@yahoo.com>
2020-10-19 06:17:24 +0300

    body margin 0

---
+++ js/sociallinks.js
@@ -0,0 +1,23 @@
+var website = "https://abdrb.github.io/";
+function funclinks(){
+document.getElementById("sociallinks").innerHTML += "<a href='" + website + "index.html'>Home</a></li>";
+document.getElementById("sociallinks").innerHTML += "<a href='" + website + "services.html'>Expert</a></li>";
+document.getElementById("sociallinks").innerHTML += "<a href='" + website + "about.html'>Effect</a></li>";
+document.getElementById("sociallinks").innerHTML += "<a href='" + website + "typing.html'>Learn Typing</a></li>";
+document.getElementById("sociallinks").innerHTML += "<a href='" + website + "space/space.html'>Space App</a></li>";
+document.getElementById("sociallinks").innerHTML += "<a href='" + website + "space/probe.html'>Hope Probe</a></li>";
+document.getElementById("sociallinks").innerHTML += "<a href='" + website + "space/farmer.html'>Farmer</a></li>";
+document.getElementById("sociallinks").innerHTML += "<a href='" + website + "solar/solar.html'>Solar|WattAmper Calc</a></li>";
+document.getElementById("sociallinks").innerHTML += "<a href='" + website + "cssg.html'>CSS Style Generator</a></li>";
+document.getElementById("sociallinks").innerHTML += "<a href='" + website + "feedback.html'>Feedback</a></li>";
+} 
+
+"'><i class='fa fa-codepen"></i></a>
+"'><i class='fa fa-github"></i></a>
+"'><i class='fa fa-linkedin-square"></i></a>
+"'><i class='fa fa-facebook-square"></i></a>
+"'><i class='fa fa-twitter"></i></a>
+"'><i class='fa fa-youtube"></i></a>
+"'><i class='fa fa-whatsapp"></i></a>
+"'><i class='fa fa-instagram"></i></a>
+"'><i class='fa fa-envelope"></i></a>
