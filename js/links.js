8cc82b76ec259a804f26c6562a1b90c5d74acc3c
forker <abdrb22@yahoo.com>
2021-08-10 14:59:37 +0300

    pdf

--- js/links.js
+++ js/links.js
@@ -12,7 +12,7 @@ link += "<li><a href='" + website + "space/space.html'>Space App</a></li>";
 link += "<li><a href='" + website + "solar/solar.html'>Solar|WattAmper Calc</a></li>";
 link += "<li><a href='" + website + "cssg.html'>CSS Style Generator</a></li>";
 link += "<li><a href='" + website + "feedback.html'>Feedback</a></li>";
-link += "<li><a href='" + website + "apps.html'>تطبيقات</a></li>";
+link += "<li><a href='" + website + "apps.html'>Portfolio</a></li>";
 document.getElementById("links").innerHTML=link;
 
 social += "<a href='https://codepen.io/Abdurab'><i class='fa fa-codepen'></i></a>";
