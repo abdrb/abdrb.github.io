e07827d24304f4aade5249e66583a612c6692e82
abdrb <abdrb22@gmail.com>
2021-12-13 17:25:18 +0300

    short link

--- js/links.js
+++ js/links.js
@@ -11,8 +11,8 @@ link += "<li><a href='" + website + "typing.html'>Learn Typing</a></li>";
 link += "<li><a href='" + website + "space/space.html'>Space App</a></li>";
 link += "<li><a href='" + website + "space/farmer.html'>Farmer App</a></li>";
 link += "<li><a href='" + website + "space/probe.html'>Hope Probe</a></li>";
-link += "<li><a href='" + website + "solar/solar.html'>Solar|WattAmper Calc</a></li>";
-link += "<li><a href='" + website + "cssg.html'>CSS Style Generator</a></li>";
+link += "<li><a href='" + website + "solar/solar.html'>Solar|W.A. Calc</a></li>";
+link += "<li><a href='" + website + "cssg.html'>CSS Style</a></li>";
 link += "<li><a href='" + website + "feedback.html'>Feedback</a></li>";
 link += "<li><a href='" + website + "apps.html'>Portfolio</a></li>";
 
